bda4d5c9e82509221f1d3172aedf50f8a2069d0d
k-ente <126145871+k-ente@users.noreply.github.com>
2024-11-26 23:01:57 +0900

    完成

--- main.go
+++ main.go
@@ -2,7 +2,30 @@ package main
 
 import "fmt"
 
+func rutine1(ch chan<- int) {
+	for i := range 15 {
+		fmt.Println("i:", i)
+		i++
+		if i%3 == 0 {
+			ch <- i
+		}
+	}
+}
+
+func rutine2(ch <-chan int) {
+	for i := range 15 {
+		i++
+		k := <-ch
+		fmt.Println("k:", k)
+	}
+
+}
+
 func main() {
-	fmt.Println("test")
+	fmt.Print("start")
+	t := make(chan int)
+	go rutine1(t)
+	rutine2(t)
+	fmt.Print("end")
 
 }
